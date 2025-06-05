307c4184070e4093995c8ed1f0391eaca567165b
MW0z <mw.kryptix@gmail.com>
2025-06-05 22:21:36 +0100

    Finished Moving average 50 + Finished PnL Calculations

--- main.cpp
+++ main.cpp
@@ -28,73 +28,102 @@ struct Trade {
     bool isBestMatch;
 };
 
+// Fixed size investment and full exit.
+
 int main() {
 
     // Reading the CSV File
     std::ifstream file("BTCUSDT-trades-2025-05-20.csv");
     std::string line, value;
-    std::deque<Trade> ma50; // Moving Average 50.
-    double currentAverage50;
+    std::deque<Trade> ma50; // Moving Average 50 store.
+    double currentAverage50 = 0; // Current Average.
+    double runningSum = 0; // Current total.
+
+    const double initalCapital = 100000;
+    double currentCapital = initalCapital; // simulation capital (starting with $10,000)
+    double fixedSizeInvestment = 1000;
+
+    double buyQuantity = 0;
+    double quantity = 0; // initial quantity is set to 0.
+    // We are using a simple fixed size investment.
+
+
+    double realized_pnl = 0; // finding the realised profit and loss
+    double unrealized_pnl = 0; // finding the unrealised profit and loss
+
 
     // Returns the first 100 values from our csv file.
-    for (int i = 0; i<100;i++){
+    for (int i = 0; i<1000;i++){
         std::getline(file, line);
         std::stringstream s (line);
         
+        // Parsing the individual line to get all necessary information.
         Trade t;
         std::getline(s, value, ',');
         t.tradeID = std::stoll(value);
-
         std::getline(s, value, ',');
         t.price = std::stod(value);
-
         std::getline(s, value, ',');
         t.quantity = std::stod(value);
-
         std::getline(s, value, ',');
         t.quoteQuantity = std::stod(value);
-
         std::getline(s, value, ',');
         t.timestamp = std::stoll(value);
-
         std::getline(s, value, ',');
-        if (value == "True"){
-            t.isBuyerMaker = true;
+        if (value == "True"){t.isBuyerMaker = true;}
+        else{t.isBuyerMaker = false;}
+        std::getline(s, value, ',');
+        if (value == "True"){t.isBestMatch = true;}
+        else{t.isBestMatch = false;}
+
+        
+        ma50.push_back(t);
+        runningSum = runningSum + (t.price);
+
+        if(ma50.size()>50){
+            Trade poppedValue = ma50.front();
+            runningSum = runningSum - poppedValue.price;
+            ma50.pop_front();
+        }
+        currentAverage50 = runningSum/ma50.size();
+        std:: cout << currentAverage50 << std::endl;
+
+        std::cout << "Trade ID: " << t.tradeID
+        << ", Price: " << t.price
+        << ", Quantity: " << t.quantity
+        << ", Quote Quantity: " << t.quoteQuantity
+        << ", Timestamp: " << t.timestamp
+        << ", Buyer Maker: " << (t.isBuyerMaker ? "True" : "False")
+        << ", Best Match: " << (t.isBestMatch ? "True" : "False")
+        << std::endl;
+
+        if (t.price>currentAverage50){
+            std::cout << "Sell" << std::endl;
+            currentCapital = currentCapital + quantity * t.price;
+            quantity = 0;
         }
         else{
-            t.isBuyerMaker = false;
+            std::cout << "Buy" << std::endl;
+            currentCapital = currentCapital - fixedSizeInvestment;
+            buyQuantity = fixedSizeInvestment/t.price;
+            quantity = quantity + buyQuantity;
         }
+        std::cout << "Current Capital = " <<  currentCapital << std::endl;
         
 
-        std::getline(s, value, ',');
-        if (value == "True"){
-            t.isBestMatch = true;
-        }
-        else{
-            t.isBestMatch = false;
-        }
-
-        ma50.push_back(t);
-        if(ma50.size()>50){
-            ma50.pop_front();
-        }        
-
     }
 
-    for (Trade t : ma50) {
-        std::cout << "Trade ID: " << t.tradeID
-                << ", Price: " << t.price
-                << ", Quantity: " << t.quantity
-                << ", Quote Quantity: " << t.quoteQuantity
-                << ", Timestamp: " << t.timestamp
-                << ", Buyer Maker: " << (t.isBuyerMaker ? "True" : "False")
-                << ", Best Match: " << (t.isBestMatch ? "True" : "False")
-                << std::endl;
-    }
 
-    std::cout << ma50.size();
+    std::cout << "Size of ma50: "<<ma50.size() << std::endl;
     
+    std::cout << "Current average price of last 50: " <<currentAverage50 << std:: endl;
 
+    double currentTotalInPurchases = quantity * ma50.back().price;
+    std::cout << "Current value in Stock: " << currentTotalInPurchases << std::endl;
+    // std::cout << capital;
+
+    std::cout << "Current total: " << currentCapital + currentTotalInPurchases << std:: endl;
+    std::cout << "Total profit/loss: $" << (currentCapital + currentTotalInPurchases) - initalCapital << std::endl;
     return 0;
 }
 
